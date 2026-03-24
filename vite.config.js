d8e950fabd5e52f797a808daff685cd249165b83
devsayanad <sayanad151@gmail.com>
2026-03-24 17:24:11 +0545

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,16 +3,15 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
+  
+  base: '/wedding-photography-frontend/', 
+  
   plugins: [react()],
   resolve: {
     alias: {
-      // Alias for components folder
       '@components': path.resolve(__dirname, 'src/components'),
-      // Alias for pages folder
       '@pages': path.resolve(__dirname, 'src/pages'),
-      // Alias for assets folder (images, styles, etc.)
       '@assets': path.resolve(__dirname, 'src/assets'),
-      // Alias for common utilities or helpers
       '@utils': path.resolve(__dirname, 'src/utils'),
     },
   },
